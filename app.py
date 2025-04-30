e64a5a66350bef6757bd6513fca7d219488ba252
BENJAMIN33760 <tola.huanca,angel@itdonbosco.org>
2025-04-30 13:29:46 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/tasks')
+def list_tasks():
+    tareas = ["Lavar la ropa", "Limpiar la casa", "Hacer la compra", "Estudiar para el examen", "Hacer ejercicio", "Leer un libro"]
+    return render_template('tasks.html', tareas=tareas)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5001)
